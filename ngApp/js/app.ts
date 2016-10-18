09ff7cbf9cd51d95121e4f8c7dc828eaa36b3efe
Brionna White <Brionnanwhite@gmail.com>
2016-10-17 17:32:10 -0700

    landing page

--- ngApp/js/app.ts
+++ ngApp/js/app.ts
@@ -1,6 +1,6 @@
 'use strict';
 namespace app {
-  angular.module('app', ['ui.router', 'ngResource', 'ui.bootstrap', 'angular-filepicker'])
+  angular.module('app', ['ui.router', 'ngResource', 'ui.bootstrap', 'angular-filepicker', 'angular-typed'])
     .config((
     $stateProvider: ng.ui.IStateProvider,
     $locationProvider: ng.ILocationProvider,
